639a3042c9d7bf6cceb6dba350c0a14b08c2ee11
eauthentik <ernosalo14@gmail.com>
2019-01-06 18:11:20 +0200

    base

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const inputValue = document.querySelector("#search");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer = document.querySelector(".main__profile-username");
+const reposContainer = document.querySelector(".main__profile-repos");
+const urlContainer = document.querySelector(".main__profile-url");
+
+// Using the application's tokens
+const client_id = "Iv1.ca05a9878764193f";
+const client_secret = "d8dd5d4d24c03db27fd3e1911fbfcc8d1a2b0b60";
