2d40b1f66aed1292f1435ed1558c84e0d2df226b
endormi <39559256+endormi@users.noreply.github.com>
2019-01-08 09:26:50 +0200

    token ff

--- Starter_files/js/app.js
+++ Starter_files/js/app.js
@@ -4,7 +4,3 @@ const nameContainer = document.querySelector(".main__profile-name");
 const unContainer = document.querySelector(".main__profile-username");
 const reposContainer = document.querySelector(".main__profile-repos");
 const urlContainer = document.querySelector(".main__profile-url");
-
-// Using the application's tokens
-const client_id = "your_tokens";
-const client_secret = "your_tokens";
